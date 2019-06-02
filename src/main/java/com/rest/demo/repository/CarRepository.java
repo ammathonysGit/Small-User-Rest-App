07123c4673ec0c5493143eff3ee862c6113e2bf1
vvasilev <ammathonys@gmail.com>
2019-06-02 13:41:39 +0300

    Added Repositry annotation in the CarRepo interface and changed docker-compose yaml file to connect with the custom made dokcer network

--- src/main/java/com/rest/demo/repository/CarRepository.java
+++ src/main/java/com/rest/demo/repository/CarRepository.java
@@ -2,10 +2,12 @@ package com.rest.demo.repository;
 
 import com.rest.demo.entity.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
+@Repository
 public interface CarRepository extends CrudRepository<Car, String> {
 
     List<Car> findByBrand(String brand);
